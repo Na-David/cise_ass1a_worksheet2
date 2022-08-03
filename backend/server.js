a9cee9dd01da8fbccdaf2194b70e15c23db043ab
David Na <hna206@naver.com>
2022-08-03 13:18:43 +1200

    second commit

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT||5001;
 
+server.get('/', (req,res) => {
+    res.send("API is running")
+})
+
+server.get('/api/articles', (req,res) =>{
+    res.json(articles);
+})
+
 server.get('/api/articles/:id', (req,res) =>{
     const article = articles.find((n) => n._id === req.params.id);
-    res.json(articles);
+    res.json(article);
     console.log(req.params);
 })
 
-server.listen(PORT, console.log("server is working and listening PORT ${PORT}"));
+server.listen(PORT, console.log(`server is working and listening PORT ${PORT}`));
