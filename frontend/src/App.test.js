5a8546a7418d12a83400f6d3cab07bed57856180
David Na <hna206@naver.com>
2022-08-03 13:33:28 +1200

    npm test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,4 @@
+/*
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,3 +7,13 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
+import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+
+  it('renders without crashing', () =>{
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
