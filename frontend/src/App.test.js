6472c3e8cf28fc0c556f73e07f6ab23b96475c1f
David Na <hna206@naver.com>
2022-08-03 13:37:49 +1200

    Basic test method added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -17,3 +17,9 @@ import React from 'react';
     ReactDOM.render(<App/>, div);
     ReactDOM.unmountComponentAtNode(div);
   })
+
+  describe('Addition', () =>{
+    it('knowns that 2 and 2 make 4', () =>{
+      expect(2+2).toBe(4);
+    });
+  });
